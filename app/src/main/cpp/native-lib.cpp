b613fe5b1a367ad55e1954b377a4455cdbd39fc0
이혜란 <ihyelan@ihyelan-ui-MacBookPro.local>
2019-06-08 10:44:28 +0900

    camera use

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,22 +4,13 @@
 
 using namespace cv;
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_ihyelan_opencvuse_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_tistory_webnautes_useopencvwithcmake_MainActivity_ConvertRGBtoGray(JNIEnv *env,
-                                                                            jobject instance,
-                                                                            jlong matAddrInput,
-                                                                            jlong matAddrResult) {
+Java_com_example_ihyelan_opencvuse_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject instance,
+                                                                 jlong matAddrInput,
+                                                                 jlong matAddrResult) {
 
- // 입력 RGBA 이미지를 GRAY 이미지로 변환
+    // 입력 RGBA 이미지를 GRAY 이미지로 변환
     Mat &matInput = *(Mat *)matAddrInput;
     Mat &matResult = *(Mat *)matAddrResult;
 
